a5fe273e391cde3c4a56920160dea23327dbd8fa
kelvin kipruto <kelvin.kipruto@compulynx.com>
2023-08-30 15:15:10 +0300

    prop edit

--- src/main/java/com/managers/app/models/Property.java
+++ src/main/java/com/managers/app/models/Property.java
@@ -5,14 +5,14 @@ import lombok.NoArgsConstructor;
 
 import jakarta.persistence.*;
 import java.util.List;
+import java.util.Random;
 
 @Entity
 @Data
 @NoArgsConstructor
 public class Property {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long propertyId;
+    private String propertyId;
 
     private String propertyName;
     private String address;
@@ -21,11 +21,26 @@ public class Property {
 
     @OneToMany(mappedBy = "property", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Building> buildings;
+    
+    private String generatePropertyId() {
+        String prefix = "PROP";
+        String codes = "0123456789";
+        StringBuilder codeBuilder = new StringBuilder();
+        Random random = new Random();
+
+        for (int i = 0; i < 3; i++) {
+            codeBuilder.append(codes.charAt(random.nextInt(codes.length())));
+        }
+
+        return prefix + codeBuilder.toString();
+    }
 
     public Property(String propertyName, String address, String type,String location) {
         this.propertyName = propertyName;
         this.address = address;
         this.type = type;
         this.location= location;
+        this.propertyId = generatePropertyId();
+
     }
 }
