21a01bc6d6a7cc361dd9aaba5ea7ed0fd3d75e85
kelvin kipruto <kelvin.kipruto@compulynx.com>
2023-08-30 15:59:34 +0300

    user modification

--- src/main/java/com/managers/app/models/User.java
+++ src/main/java/com/managers/app/models/User.java
@@ -1,6 +1,7 @@
 package com.managers.app.models;
 
 import lombok.Data;
+
 import lombok.NoArgsConstructor;
 
 import jakarta.persistence.Entity;
@@ -8,13 +9,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.sql.Date;
+import java.util.Calendar;
+import java.util.Random;
 
 @Entity
 @Data
 @NoArgsConstructor
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String username;
@@ -23,6 +25,13 @@ public class User {
     private boolean isActive;
     private Date registrationDate;
     private Date lastLogin;
+    
+    private Long generateUniqueId() {
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR) % 100; // Get last two digits of the year
+        Random random = new Random();
+        int randomSuffix = random.nextInt(90000) + 10000; // Generates a random number between 10000 and 99999
+        return Long.parseLong(currentYear + "" + randomSuffix);
+    }
 
     public User(String username, String email, String passwordHash, boolean isActive) {
         this.username = username;
@@ -31,7 +40,12 @@ public class User {
         this.isActive = isActive;
         this.registrationDate = new Date(System.currentTimeMillis());
         this.lastLogin = new Date(System.currentTimeMillis());
+        this.id = generateUniqueId();
+
     }
+    
+    
+    
 
     // Getters and Setters
     public Long getId() {
